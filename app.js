14c39859acbdced2ac3e30c5fd31fd1cce07a5e0
Raman Kalita <raman_kalita@epam.com>
2025-05-24 18:56:20 +0200

    Test deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Elastic Beanstalk!");
+  res.send("Hello from Elastic Beanstalk! + deploy");
 });
 
 app.listen(port, () => {
