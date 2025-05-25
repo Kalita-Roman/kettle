eefbfb33a103912f6f9376c7e536043687ee4c14
Raman Kalita <raman_kalita@epam.com>
2025-05-25 13:53:47 +0200

    Use JSON

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Elastic Beanstalk! And deploy");
+  res.json({ message: "XXX" });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.get("/dmp6jh-3000", (req, res) => {
+  res.json({ message: "dmp6jh-3000" });
+});
+
+app.get("/*", (req, res) => {
+  res.json({ message: "The rest" });
+});
+
+app.listen(80, () => {
+  console.log(`Example app listening on port ${port}`);
 });
